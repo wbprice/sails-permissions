84ff25261f85fdbff36049e38d69b3f8d1ed22bc
Blaine Price <1wbprice@gmail.com>
2015-10-28 16:59:28 -0400

    Create and delete some mock users to be returned.

--- test/unit/services/AdminService.test.js
+++ test/unit/services/AdminService.test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var Promise = require('bluebird');
 
 describe('Admin Service', function() {
 
@@ -9,22 +10,40 @@ describe('Admin Service', function() {
 
   describe('#getUser()', function() {
 
-    beforeEach(function() {
-      User.create({
-        username: 'Phillip'
+    beforeEach(function(done) {
+
+      Promise.all([
+        User.create({
+          username: 'Phillip'
+        }),
+        User.create({
+          username: 'Georgia'
+        }),
+        User.create({
+          username: 'Hiro Protaganist'
+        })
+      ]).spread(function() {
+        done();
       });
-      User.create({
-        username: 'Georgia'
-      });
-      User.create({
-        username: 'Hiro Protaganist'
-      });
+
     });
 
     afterEach(function(done) {
-      User.destroy().exec(function() {
+
+      Promise.all([
+        User.destroy({
+          username: 'Phillip'
+        }),
+        User.destroy({
+          username: 'Georgia'
+        }),
+        User.destroy({
+          username: 'Hiro Protaganist'
+        })
+      ]).spread(function() {
         done();
       });
+
     });
 
     it('should exist', function() {
@@ -33,6 +52,11 @@ describe('Admin Service', function() {
 
     it('should return all the users, with populated Role(s)', function() {
 
+      var output = User.find().exec(function(users) {
+        return users;
+      });
+
+      console.log(output);
 
     });
 
